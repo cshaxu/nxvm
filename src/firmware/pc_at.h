e6482841659029624003599ed8fee6a31ccba9a0
cshaxu <cshaxu@gmail.com>
2026-07-31 15:12:04 -0700

    M5 T4 S2 P1: add pc at firmware image

--- src/firmware/pc_at.h
+++ src/firmware/pc_at.h
@@ -1,6 +1,7 @@
 #ifndef NXVM_FIRMWARE_PC_AT_H
 #define NXVM_FIRMWARE_PC_AT_H
 
+#include "core/machine.h"
 #include "firmware/firmware.h"
 
 typedef struct nxvm_firmware_pc_at_plan {
@@ -11,5 +12,7 @@ typedef struct nxvm_firmware_pc_at_plan {
 
 nxvm_core_status nxvm_firmware_pc_at_compose(
     nxvm_firmware *firmware, nxvm_firmware_pc_at_plan *out_plan);
+nxvm_core_status nxvm_firmware_pc_at_apply_image(
+    nxvm_core_machine *machine, int boot_hdd);
 
 #endif
