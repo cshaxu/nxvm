691fc526972a4f0409bf328f5ee21965cc92a416
cshaxu <cshaxu@gmail.com>
2026-07-31 22:11:31 -0700

    M5 T10 S3 P9: relocate core contract foundations

--- src/core/lifecycle.h
+++ src/core/lifecycle.h
@@ -1,20 +1,2 @@
-#ifndef NXVM_CORE_LIFECYCLE_H
-#define NXVM_CORE_LIFECYCLE_H
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-typedef enum nxvm_core_machine_lifecycle {
-    NXVM_CORE_MACHINE_INITIALIZED = 0,
-    NXVM_CORE_MACHINE_PAUSED,
-    NXVM_CORE_MACHINE_RUNNING,
-    NXVM_CORE_MACHINE_STOPPED,
-    NXVM_CORE_MACHINE_FAULTED
-} nxvm_core_machine_lifecycle;
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif
+/* Temporary M5 forwarding header. The contract moved to machine/core. */
+#include "machine/core/contract/lifecycle.h"
--- src/core/status.h
+++ src/core/status.h
@@ -1,23 +1,2 @@
-#ifndef NXVM_CORE_STATUS_H
-#define NXVM_CORE_STATUS_H
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-#define NXVM_CORE_ABI_VERSION 1u
-
-typedef enum nxvm_core_status {
-    NXVM_CORE_STATUS_OK = 0,
-    NXVM_CORE_STATUS_INVALID_ARGUMENT,
-    NXVM_CORE_STATUS_INVALID_STATE,
-    NXVM_CORE_STATUS_UNSUPPORTED,
-    NXVM_CORE_STATUS_NO_MEMORY,
-    NXVM_CORE_STATUS_FAULT
-} nxvm_core_status;
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif
+/* Temporary M5 forwarding header. The contract moved to machine/core. */
+#include "machine/core/contract/status.h"
