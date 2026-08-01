d68e03a6f03479c8559a5b0be222e0d1dab9de42
cshaxu <cshaxu@gmail.com>
2026-08-01 02:49:33 -0700

    M5 T12 S9 P57: move NXVM request transport

--- src/vm/platform/baseline_request_transport.c
+++ src/vm/platform/baseline_request_transport.c
@@ -1,4 +1,4 @@
-#include "adapters/nxvm_baseline/vm_request_transport.h"
+#include "vm/platform/baseline_request_transport.h"
 
 static void nxvm_baseline_vm_request_transport_lock(
     nxvm_baseline_vm_request_transport *transport)
