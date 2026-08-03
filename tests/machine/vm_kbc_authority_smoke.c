9f00d5bc382ae31fdc417a82915f61bd5015cc1d
cshaxu <cshaxu@gmail.com>
2026-08-02 17:59:23 -0700

    M5 T86 S1 P1: close NXVM executor routes

--- tests/machine/vm_kbc_authority_smoke.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "core/machine/kbc.h"
-#include "vm/composition_live_machine.h"
-#include "vm/composition_machine.h"
-int main(void)
-{
-    vm_composition_live_machine session = {0};const vm_composition_live_machine *m; machineInit(&session);m=(&session);if(!m||m->kbc!=&m->kbc_storage){machineFinal(&session);return 1;}machineFinal(&session);puts("M5:T30:S1:KBC-AUTHORITY:OK");return 0;}
