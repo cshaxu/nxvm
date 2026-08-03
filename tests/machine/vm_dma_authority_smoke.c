6b7ce7d833f8c7ad575ab8a880f3594300a9537a
cshaxu <cshaxu@gmail.com>
2026-08-02 17:59:23 -0700

    M5 T86 S1 P1: close NXVM executor routes

--- tests/machine/vm_dma_authority_smoke.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "core/machine/dma.h"
-#include "vm/composition_live_machine.h"
-#include "vm/composition_machine.h"
-int main(void)
-{
-    vm_composition_live_machine session = {0}; const vm_composition_live_machine *m; machineInit(&session); m=(&session); if(!m || m->dma_primary != &m->dma_primary_storage || m->dma_secondary != &m->dma_secondary_storage || m->dma_latch != &m->dma_latch_storage) { machineFinal(&session); return 1; } machineFinal(&session); puts("M5:T29:S1:DMA-AUTHORITY:OK"); return 0; }
